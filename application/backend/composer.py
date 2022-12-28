eff0e3a2c90a4eae910feaf53586ddc5918ca69d
Nadav Tasher <tashernadav@gmail.com>
2022-12-28 17:42:11 +0000

    WIP

--- application/backend/composer.py
+++ application/backend/composer.py
@@ -18,11 +18,20 @@ Data = Database("/opt/database.json")
 Token = Authority(os.environ.get("SECRET").encode())
 Deployment = collections.namedtuple("Deployment", ["id", "path", "name", "directory", "repository"])
 
+
+def TokenType(token):
+    validate(password, Text)
+
+    # Validate the token
+    Token.validate(token)
+
+
 def PasswordType(password):
     validate(password, Text)
 
     # Make sure password is valid
     assert password == os.environ.get("PASSWORD"), "Password is invalid!"
+
 
 def DeploymentType(identifier):
     validate(identifier, Text)
@@ -31,12 +40,14 @@ def DeploymentType(identifier):
     database = Data.read()
     assert identifier in database.keys(), "Invalid deployment ID"
 
+
 def load(identifier):
     # Read the database
     database = Data.read(identifier)
 
     # Read the deployment
     return Deployment(id=identifier, path=os.path.join(OUTPUT, identifier), **database[identifier])
+
 
 def format(template, deployment):
     # Create deployment dict with escaped parameters
@@ -50,12 +61,35 @@ def format(template, deployment):
     # Format with parameters
     return template.format(**parameters)
 
-@router.post("/list")
+def action(name, token, setup=True):
+    # Parse token object and get ID
+    token = Token.validate(token, name)
+
+    # Load the deployment
+    deployment = load(token.contents["id"])
+
+    # Make sure the deployment was set-up
+    assert deployment.repository, "Deployment repository was not set"
+    assert deployment.directory, "Deployment directory was not set"
+
+    # Check if repository is set-up
+    exists = os.path.exists(os.path.join(deployment.path, REPOSITORY))
+
+    # Make sure setup requirements are met
+    if setup:
+        assert exists, "Deployment repository is not initialized"
+    else:
+        assert not exists, "Deployment repository is already initialized"
+
+    # Execute and evaluate TODO
+
+@router.post("list")
 @kwargcheck(password=PasswordType)
 def _list(request, password=None):
     return Data.read()
 
-@router.post("/new")
+
+@router.post("new")
 @kwargcheck(password=PasswordType)
 def _new(request, password=None):
     # Create new deployment identifier
@@ -76,7 +110,8 @@ def _new(request, password=None):
     # Return the new ID
     return identifier
 
-@router.post("/fetch")
+
+@router.post("fetch")
 @kwargcheck(password=PasswordType, identifier=DeploymentType)
 def _fetch(request, password=None, identifier=None):
     # Read deployment SSH key
@@ -88,29 +123,29 @@ def _fetch(request, password=None, identifier=None):
 
     # Fetch general information about deployment
     deployment = load(identifier)
-    
+
     # Return all information
     return dict(key=key.decode(), token=token.decode(), **deployment._asdict())
 
-@router.post("/token")
+
+@router.post("token")
 @kwargcheck(password=PasswordType, identifier=DeploymentType)
 def _token(request, password=None, identifier=None):
     # Issue token with limited permissions
-    token, _ = Token.issue(
-        "Permenant access token for %s" % identifier, dict(id=identifier), [
-						"pull",
-						"stop",
-						"start",
-						"update",
-						"restart",
-						"webhook",
-					], 
-                    10 * 60 * 60 * 24 * 365)
+    token, _ = Token.issue("Permenant access token for %s" % identifier, dict(id=identifier), [
+        "pull",
+        "stop",
+        "start",
+        "update",
+        "restart",
+        "webhook",
+    ], 10 * 60 * 60 * 24 * 365)
 
     # Return the created token
     return token
 
-@router.post("/update")
+
+@router.post("update")
 @kwargcheck(password=PasswordType, identifier=DeploymentType, name=str, directory=str, repository=str)
 def _update(request, password=None, identifier=None, name=None, directory=None, repository=None):
     # Read database and update deployment
@@ -120,7 +155,8 @@ def _update(request, password=None, identifier=None, name=None, directory=None, 
     # Write the database
     Data.write(database)
 
-@router.post("/delete")
+
+@router.post("delete")
 @kwargcheck(password=PasswordType, identifier=DeploymentType)
 def _delete(request, password=None, identifier=None):
     # Load deployment from database
@@ -138,3 +174,12 @@ def _delete(request, password=None, identifier=None):
     database = Data.read()
     database.pop(identifier)
     Data.write(database)
+
+
+@router.post("clone")
+@kwargcheck(token=TokenType)
+def _clone(request, token=None):
+    pass
+
+
+# yapf -ir . --style "{based_on_style: google, column_limit: 400, indent_width: 4}"
