6ebb495deb05da19c462fccc5c94c907d462248b
Nadav Tasher <tashernadav@gmail.com>
2023-01-06 07:26:05 +0000

    Documentation

--- application/backend/composer.py
+++ application/backend/composer.py
@@ -39,6 +39,10 @@ def DeploymentType(identifier):
 
 @future
 def evaluate(command, identifier=None, **parameters):
+    """
+    Formats a command with the given parameters (and changed working directory)
+    """
+
     # Escape parameters for execution
     escaped_parameters = {
         # Escape using JSON module
@@ -51,13 +55,17 @@ def evaluate(command, identifier=None, **parameters):
     rendered_command = command.format(identifier=identifier, **escaped_parameters)
 
     # Change CWD of execution
-    cwd_command = f"cd {OUTPUT}; cd {identifier}; %s" % rendered_command
+    cwd_command = f"cd {OUTPUT} && cd {identifier} && %s" % rendered_command
 
     # Execute command and return output
     return execute(cwd_command)
 
 
 def register(action, command, setup, asyncronous):
+    """
+    Registers a deployment action
+    An action is a command that gets formatted with parameters
+    """
 
     @router.post(action)
     @kwargcheck(token=TokenType)
@@ -86,12 +94,20 @@ def register(action, command, setup, asyncronous):
 @router.post("list")
 @kwargcheck(password=PasswordType)
 def _list(request, password):
+    """
+    Lists all deployment with configuration values
+    """
+
     return Data.read()
 
 
 @router.post("info")
 @kwargcheck(password=PasswordType, identifier=DeploymentType)
 def _info(request, password, identifier):
+    """
+    Fetches extended information about a deployment
+    """
+
     # Read deployment SSH key
     with open(os.path.join(OUTPUT, identifier, PUBLIC), "rb") as key_file:
         key = key_file.read()
@@ -106,6 +122,12 @@ def _info(request, password, identifier):
 @router.post("token")
 @kwargcheck(password=PasswordType, identifier=DeploymentType)
 def _token(request, password, identifier):
+    """
+    Generates two permenant access tokens
+    1. General access token (log, update, restart, etc.)
+    2. Webhook access token (Async restart)
+    """
+
     # Issue token with general permissions
     general, _ = Token.issue("General token", dict(id=identifier), [
         "log",
@@ -126,6 +148,10 @@ def _token(request, password, identifier):
 @router.post("new")
 @kwargcheck(password=PasswordType)
 def _new(request, password):
+    """
+    Creates a new deployment
+    """
+
     # Create new deployment identifier
     identifier = binascii.b2a_hex(os.urandom(4)).decode()
 
@@ -145,12 +171,20 @@ def _new(request, password):
 @router.post("edit")
 @kwargcheck(password=PasswordType, identifier=DeploymentType, name=Text, directory=Text, repository=Text)
 def _edit(request, password, identifier, name, directory, repository):
+    """
+    Edits an existing deployment
+    """
+
     Data.set(identifier, dict(name=name, directory=directory, repository=repository))
 
 
 @router.post("delete")
 @kwargcheck(password=PasswordType, identifier=DeploymentType)
 def _delete(request, password, identifier):
+    """
+    Deletes an existing deployment
+    """
+
     # Check if repository was cloned
     if os.path.exists(os.path.join(OUTPUT, identifier, REPOSITORY)):
         # Destroy the deployment
